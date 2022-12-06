430839e14729084726dd1bc7523ee7c31e8f4ead
root <o.olurinola@alustudent.com>
2022-12-06 13:14:23 +0200

    task 1

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# find and fix 500 error, automate using Puppet
+exec {'fix500':
+  command => 'sed -i "s|require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );\
+|require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );|g" /var/www/html/wp-settings.php',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
+}
